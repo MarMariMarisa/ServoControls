09698022ff3a0962849bd7a6a82b15cb91839313
MarMariMarisa <96095835+MarMariMarisa@users.noreply.github.com>
2024-02-25 21:49:50 -0500

    start of P1

---
+++ Core/Inc/printf.h
@@ -0,0 +1,17 @@
+/*
+ * printf.h
+ *
+ *  Created on: Sep 12, 2023
+ *      Author: Marisa
+ */
+
+#ifndef INC_SYSTEM_PRINTF_H_
+#define INC_SYSTEM_PRINTF_H_
+
+int puts (const char* string);
+
+int printf(const char*,...);
+int putchar (int chr);
+
+
+#endif /* INC_SYSTEM_PRINTF_H_ */
---
+++ Core/Src/GPIO.c
@@ -0,0 +1,53 @@
+/*
+ * gpio.c
+ *
+ *  Created on: Nov 4, 2021
+ *      Author: Mitesh Parikh
+ */
+
+
+/* Includes ------------------------------------------------------------------*/
+#include "GPIO.h"
+#include <stdbool.h>
+#include "stm32l4xx.h"
+
+// External Global Variables that we will need access to
+
+
+/*----------------------------------------------------------------------------*/
+/* Configure GPIO                                                             */
+/*----------------------------------------------------------------------------*/
+void GPIO_Init(void)
+{
+	GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+	/* GPIO Ports Clock Enable */
+	__HAL_RCC_GPIOC_CLK_ENABLE();
+	__HAL_RCC_GPIOH_CLK_ENABLE();
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+	__HAL_RCC_GPIOB_CLK_ENABLE();
+	__HAL_RCC_GPIOA_CLK_ENABLE(); // Enable GPIOA clock
+	__HAL_RCC_GPIOA_CLK_ENABLE(); // Enable GPIOA clock
+	__HAL_RCC_GPIOA_CLK_ENABLE(); // Enable GPIOA clock
+	__HAL_RCC_TIM2_CLK_ENABLE();  // Enable TIM2 clock	/*Configure GPIO pin : PtPin */
+	GPIO_InitStruct.Pin = B1_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+
+	GPIO_InitStruct.Pin = S1_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING_FALLING;
+	GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+	HAL_GPIO_Init(S1_GPIO_Port, &GPIO_InitStruct);
+
+	GPIO_InitStruct.Pin = GPIO_PIN_0;
+	GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+	GPIO_InitStruct.Alternate = GPIO_AF1_TIM2;
+
+	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+
+}
+
